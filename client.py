b3f286110582d77c52694347a1b1c230855b3696
Emre Kuru <93038341+emrekuruu@users.noreply.github.com>
2022-12-03 23:05:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,10 +2,18 @@ from socket import *
 import threading
 
 serverName = "127.0.0.1"
-port = 22010
+port = 22011
 
 #Get a Nick name from the client
-nickName = input("What would you like to be called: ")
+print("INFO PLEASE READ BEFORE CHATTING")
+print("For public messages start your messages with the keyword PUBLIC")
+print("Example: PUBLIC Hello World")
+print("\nFor private messages please write the nickname of the person you want to message at the start of your message")
+print(("Example: Emre Hello World"))
+print("\nIf you wish to leave the chat please enter EXIT")
+
+
+nickName = input("\nWhat would you like to be called: ")
 
 #Connection syntax
 clientSocket = socket(AF_INET,SOCK_STREAM)
@@ -31,8 +39,12 @@ def receive():
 #This is for writing in the chat
 def write():
     while True:
-        messageToBeSent = f'{nickName}: {input("")}'
+        inp = input("")
+        messageToBeSent = f'{nickName}: {inp}'
         clientSocket.send(messageToBeSent.encode())
+        if(inp == "EXIT"):
+            clientSocket.close()
+            break
 
 
 #This is the tricky part !!!
@@ -42,5 +54,6 @@ receiveThread.start()
 
 writeThread = threading.Thread(target=write)
 writeThread.start()
+
 
 
