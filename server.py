186993824af0d03d6a681515355a04a0ac7bfeb4
Emre Kuru <93038341+emrekuruu@users.noreply.github.com>
2022-12-28 12:29:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from socket import *
 import threading
 
 host = "127.0.0.1"
-port = 22011
+port = 22034
 
 serverSocket = socket(AF_INET,SOCK_STREAM )
 serverSocket.bind((host,port))
@@ -11,9 +11,18 @@ print("Server is now ready to accept connections")
 
 #Clients list
 clients = []
+client_dick = dict()
+
+#Passwords List
+passwords = dict()
+passwords["admin"] = "admin123"
 
 #Nickname list
 nickNames = []
+nickNames.append("admin")
+
+#Banned accounts
+banned = []
 
 #for sending a message to each client
 def broadcast(message):
@@ -28,16 +37,28 @@ def handle(client):
           message = client.recv(1024)
           decodedMessage = message.decode()
           myList = decodedMessage.split(" ")
+          print(myList)
 
-          if(myList[1] == "EXIT"):
-                index = clients.index(client)
+          if(myList[0] == "/ban"):
+              print("An account is Banned")
+              nickNameToBeBanned = myList[1]
+              banned.append(nickNameToBeBanned)
+              for client_ in client_dick.keys():
+                  if client_dick[client_] == nickNameToBeBanned:
+                     client_.send("REFUSED".encode())
+                     client_.close()
+                     clients.remove(client_)
+
+              broadcast(f"{nickNameToBeBanned} is now banned".encode())
+
+
+          elif(myList[1] == "EXIT"):
                 clients.remove(client)
                 client.close()
-                broadcast(f"{nickNames[index]} has left the chat".encode())
-                nickNames.remove(nickNames[index])
+                broadcast(f"{client_dick[client]} has left the chat".encode())
 
           #Were checking if this message is for the public or its a private message!
-          if(myList[1] == "PUBLIC"):
+          elif(myList[1] == "PUBLIC"):
               myList.remove("PUBLIC")
               message = ""
               for i in myList:
@@ -55,31 +76,68 @@ def handle(client):
                         for i in myList:
                               message += f"{i} "
                         print(f"There is a private message to {nickname}")
-                        index = nickNames.index(nickname)
-                        wantedClient = clients[index]
-                        wantedClient.send(message.encode())
+                        for WantedClient in client_dick.keys():
+                            if client_dick[WantedClient] == nickname:
+                                WantedClient.send(message.encode())
 
         except:
-            index = clients.index(client)
             clients.remove(client)
             client.close()
-            broadcast(f"{nickNames[index]} has left the chat".encode())
-            nickNames.remove(nickNames[index])
+            broadcast(f"{client_dick[client]} has left the chat".encode())
             break
 
 
 #basiclly the main function
 def receive():
     while True:
+
         #once a client connects get its address and create a socket for them
         clientSocket,address = serverSocket.accept()
         print(f"Connected from {address}")
 
+        #Sign in or Log in?
+        clientSocket.send("SIGN".encode())
+        first_time = clientSocket.recv(1024).decode()
+
+        if(first_time == "SIGN IN"):
         #ask them for a username than add them to our lists
-        clientSocket.send("NICK".encode())
-        nickname = clientSocket.recv(1024).decode()
-        nickNames.append(nickname)
-        clients.append(clientSocket)
+            clientSocket.send("NICK".encode())
+            nickname = clientSocket.recv(1024).decode()
+        #Used nickname
+            nickname_alredy_used = True
+            if(nickNames.__contains__(nickname)):
+                    clientSocket.send("REFUSED".encode())
+                    continue
+            else:
+                    nickname_alredy_used = False
+            nickNames.append(nickname)
+            clients.append(clientSocket)
+            client_dick[clientSocket] = nickname
+
+        #ask them to pick a password
+            clientSocket.send("PASSWORD".encode())
+            password = clientSocket.recv(1024).decode()
+            passwords[nickname] = password
+
+        else:
+        #ask them their nickname to verify
+            clientSocket.send("NICK".encode())
+            nickname = clientSocket.recv(1024).decode()
+            if((nickNames.__contains__(nickname)) and nickname not in banned):
+                #Get password to verify nickname
+                clientSocket.send("PASSWORD".encode())
+                password = clientSocket.recv(1024).decode()
+                if(passwords[nickname] != password):
+                    clientSocket.send("REFUSED".encode())
+                    clientSocket.close()
+                    continue
+                else:
+                    clients.append(clientSocket)
+            else:
+                clientSocket.send("REFUSED".encode())
+                clientSocket.close()
+                continue
+
         print(f"Nickname of the client is {nickname}")
 
         #tell everyone in the chat someone has connected
