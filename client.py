df57f29d9a780c9f7f7fa2d070740b6420bbba91
Emre Kuru <93038341+emrekuruu@users.noreply.github.com>
2023-01-10 23:00:47 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,3 @@
-
-
 from socket import *
 import threading
 import tkinter
@@ -9,11 +7,10 @@ import rsa
 
 
 serverName = "127.0.0.1"
-port = 22054
+port = 22062
 public_key,private_key = rsa.newkeys(1024)
 
 class Client:
-
     def __init__(self, host, port):
         # Connection syntax
         self.clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -24,6 +21,7 @@ class Client:
 
 
         msg = tkinter.Tk()
+
         msg.withdraw()
 
         self.gui_done = False
@@ -65,10 +63,20 @@ class Client:
         self.text_area.yview("end")
         self.text_area.config(state="disabled")
 
-        # Another label
+
+        self.online_label = tkinter.Label(self.win, text="Currently Online: ", bg="lightgray")
+        self.online_label.config(font=("Arial", 12))
+        self.online_label.pack(padx=20, pady=5)
+
+        self.online_area = tkinter.scrolledtext.ScrolledText(self.win,height=3)
+        self.online_area.pack(padx=20,pady=5)
+        self.online_area.config(state="disabled")
+
+                # Another label
         self.msg_label = tkinter.Label(self.win, text="Message: ", bg="lightgray")
         self.msg_label.config(font=("Arial", 12))
         self.msg_label.pack(padx=20, pady=5)
+
 
         self.input_area = tkinter.Text(self.win, height=3)
         self.input_area.pack(padx=20, pady=5)
@@ -77,14 +85,9 @@ class Client:
         self.send_button.config(font=("Arial", 12))
         self.send_button.pack(padx=20, pady=5)
 
-
         self.public_button = tkinter.Button(self.win, text="PUBLIC",command=self.forPublic)
         self.public_button.config(font=("Arial",12))
         self.public_button.pack(padx=20,pady=5)
-
-        self.get_nicknames = tkinter.Button(self.win,text="See Who is Online",command=self.online)
-        self.get_nicknames.config(font=("Ariel",12))
-        self.get_nicknames.pack(padx=25,pady=5)
 
         self.gui_done = True
         self.win.protocol("WM_DELETE_WINDOW", self.stop)
@@ -102,6 +105,17 @@ class Client:
                 if message == "SIGN":
                     self.clientSocket.send(rsa.encrypt(self.SignIn.encode(),self.server_key))
 
+                if message == "NEW":
+                    online = rsa.decrypt(self.clientSocket.recv(1024),private_key).decode()
+                    self.online_area.config(state="normal")
+                    self.online_area.insert("end","\n")
+                    self.online_area.yview("end")
+                    self.online_area.insert("end","")
+                    self.online_area.yview("end")
+                    self.online_area.insert("end", online)
+                    self.online_area.yview("end")
+                    self.online_area.config(state="disabled")
+
                 # if the message is NICK this means server wants us to send it our nickname( this is the initial connection )
                 elif message == "NICK":
                     self.clientSocket.send(rsa.encrypt(self.nickName.encode(),self.server_key))
@@ -115,7 +129,7 @@ class Client:
                         "Try Again")
                     self.stop()
 
-                if self.gui_done:
+                elif self.gui_done:
                     self.text_area.config(state="normal")
                     self.text_area.insert("end","\n")
                     self.text_area.yview("end")
@@ -124,14 +138,17 @@ class Client:
                     self.text_area.insert("end", message)
                     self.text_area.yview("end")
                     self.text_area.config(state="disabled")
+
             except:
                 print("Connection Failed")
                 self.stop()
+
     # This is for writing in the chat
     def write(self):
         self.message += self.input_area.get("1.0", "end")
         self.input_area.delete("1.0", "end")
         self.message = self.nickName + ": " + self.message
+
             #This is for banning clients
         if self.message[len(self.nickName)+2:].startswith("/"):
             if self.nickName == "admin":
@@ -147,9 +164,6 @@ class Client:
 
     def forPublic(self):
         self.message += "PUBLIC "
-
-    def online(self):
-        self.clientSocket.send(rsa.encrypt(f"ONLINE {self.nickName}".encode(),self.server_key))
 
 
 client = Client(serverName, port)
