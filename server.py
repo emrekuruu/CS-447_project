aba720ea1a597d596b80b361f20993b2df6f9375
Emre Kuru <93038341+emrekuruu@users.noreply.github.com>
2023-01-11 11:23:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,169 +1,170 @@
 from socket import *
 import threading
-import tkinter
-from tkinter import simpledialog
-import tkinter.scrolledtext
 import rsa
 
-
-serverName = "127.0.0.1"
+host = "127.0.0.1"
 port = 22064
+
+
 public_key,private_key = rsa.newkeys(1024)
 
-class Client:
-    def __init__(self, host, port):
-        # Connection syntax
-        self.clientSocket = socket(AF_INET, SOCK_STREAM)
-        self.clientSocket.connect((host, port))
-        self.message = ""
-        self.server_key = rsa.PublicKey.load_pkcs1(self.clientSocket.recv(1024))
-        self.clientSocket.send(public_key.save_pkcs1("PEM"))
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((host, port))
+serverSocket.listen(1)
+print("Server is now ready to accept connections")
+
+# Clients list
+clients = []
+client_dick = dict()
+
+# Passwords List
+passwords = dict()
+passwords["admin"] = "admin123"
+
+# Nickname list
+nickNames = []
+nickNames.append("admin")
+
+# Banned accounts
+banned = []
+
+# Public keys of the clients
+p_key = dict()
 
 
-        msg = tkinter.Tk()
-
-        msg.withdraw()
-
-        self.gui_done = False
-
-        self.SignIn = simpledialog.askstring(("User Name"), "CREATE ACCOUNT OR LOG IN", parent=msg)
-        self.nickName = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
-
-        # Will they sign in log in
-        self.password = simpledialog.askstring(("password"), "Password", parent=msg)
-
-        self.running = True
-        self.g_thread = threading.Thread(target=self.guiloop)
-        self.r_thread = threading.Thread(target=self.receive)
-        self.g_thread.start()
-        self.r_thread.start()
-
-    def stop(self):
-        self.win.destroy()
-        self.running = False
-        self.clientSocket.close()
-
-    def guiloop(self):
-        # Window itself
-        self.win = tkinter.Tk()
-        self.win.configure(bg="lightgray")
-
-        # a small label
-        self.chat_label = tkinter.Label(self.win, text="Chat: ", bg="lightgray")
-        self.chat_label.config(font=("Arial", 12))
-        self.chat_label.pack(padx=20, pady=5)
-
-        # The area where the messages are displayed
-        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
-        self.text_area.pack(padx=20, pady=5)
-        self.text_area.config(state="normal")
-        self.text_area.insert("end","Welcome to our online Chat Room")
-        self.text_area.yview("end")
-        self.text_area.insert("end","\n")
-        self.text_area.yview("end")
-        self.text_area.config(state="disabled")
+# for sending a message to each client
+def broadcast(message):
+    for client in clients:
+        client.send(rsa.encrypt(message,p_key[client]))
 
 
-        self.online_label = tkinter.Label(self.win, text="Currently Online: ", bg="lightgray")
-        self.online_label.config(font=("Arial", 12))
-        self.online_label.pack(padx=20, pady=5)
+# business logic for one client (later this method will run for all clients)
+def handle(client):
+    while True:
+        try:
+            message = rsa.decrypt(client.recv(1024),private_key)
+            decodedMessage = message.decode()
+            myList = decodedMessage.split(" ")
+            print(myList)
 
-        self.online_area = tkinter.scrolledtext.ScrolledText(self.win,height=3)
-        self.online_area.pack(padx=20,pady=5)
-        self.online_area.config(state="disabled")
+            if (myList[0] == "/ban"):
+                print("An account is Banned")
+                nickNameToBeBanned = myList[1]
+                print(nickNameToBeBanned)
+                banned.append(nickNameToBeBanned)
+                for client_ in client_dick.keys():
+                    if client_dick[client_] == nickNameToBeBanned:
+                        client_.send(rsa.encrypt("REFUSED".encode(),p_key[client_]))
+                        client_.close()
+                        clients.remove(client_)
 
-                # Another label
-        self.msg_label = tkinter.Label(self.win, text="Message: ", bg="lightgray")
-        self.msg_label.config(font=("Arial", 12))
-        self.msg_label.pack(padx=20, pady=5)
+                broadcast(f"{nickNameToBeBanned} is now banned".encode())
 
 
-        self.input_area = tkinter.Text(self.win, height=3)
-        self.input_area.pack(padx=20, pady=5)
+            elif (myList[1] == "EXIT"):
+                clients.remove(client)
+                client.close()
+                broadcast(f"{client_dick[client]} has left the chat".encode())
 
-        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
-        self.send_button.config(font=("Arial", 12))
-        self.send_button.pack(padx=20, pady=5)
+            # Were checking if this message is for the public or its a private message!
+            elif (myList[1] == "PUBLIC"):
+                myList.remove("PUBLIC")
+                message = ""
+                for i in myList:
+                    message += f"{i} "
 
-        self.public_button = tkinter.Button(self.win, text="PUBLIC",command=self.forPublic)
-        self.public_button.config(font=("Arial",12))
-        self.public_button.pack(padx=20,pady=5)
+                print("There is a public message!")
+                broadcast(message.encode())
 
-        self.gui_done = True
-        self.win.protocol("WM_DELETE_WINDOW", self.stop)
-        self.win.mainloop()
-
-    # This is to see what everyone else wrote on the chat
-    def receive(self):
-        while self.running:
-            try:
-                message = rsa.decrypt(self.clientSocket.recv(1024),private_key).decode()
-                print(message)
-                print(self.gui_done)
-
-                # This is the sign in message
-                if message == "SIGN":
-                    self.clientSocket.send(rsa.encrypt(self.SignIn.encode(),self.server_key))
-
-                if message == "NEW":
-                    online = rsa.decrypt(self.clientSocket.recv(1024),private_key).decode()
-                    self.online_area.config(state="normal")
-                    self.online_area.insert("end","\n")
-                    self.online_area.yview("end")
-                    self.online_area.insert("end","")
-                    self.online_area.yview("end")
-                    self.online_area.insert("end", online)
-                    self.online_area.yview("end")
-                    self.online_area.config(state="disabled")
-
-                # if the message is NICK this means server wants us to send it our nickname( this is the initial connection )
-                elif message == "NICK":
-                    self.clientSocket.send(rsa.encrypt(self.nickName.encode(),self.server_key))
-
-                elif message == "PASSWORD":
-                    self.clientSocket.send(rsa.encrypt(self.password.encode(),self.server_key))
-
-                elif message == "REFUSED":
-                    print(
-                        "Server refused the connection\nEither you entered a wrong password\nPicked a username that was already used\nOr Your account is banned"
-                        "Try Again")
-                    self.stop()
-
-                elif self.gui_done:
-                    self.text_area.config(state="normal")
-                    self.text_area.insert("end","\n")
-                    self.text_area.yview("end")
-                    self.text_area.insert("end","")
-                    self.text_area.yview("end")
-                    self.text_area.insert("end", message)
-                    self.text_area.yview("end")
-                    self.text_area.config(state="disabled")
-
-            except:
-                print("Connection Failed")
-                self.stop()
-
-    # This is for writing in the chat
-    def write(self):
-        self.message += self.input_area.get("1.0", "end")
-        self.input_area.delete("1.0", "end")
-        self.message = self.nickName + ": " + self.message
-
-            #This is for banning clients
-        if self.message[len(self.nickName)+2:].startswith("/"):
-            if self.nickName == "admin":
-                self.clientSocket.send(rsa.encrypt(self.message[len(self.nickName)+2:].encode(),self.server_key))
+            # If we can match the nickname with our cleints send the message to them
             else:
-                print("Only Admins can execute commands")
+                for nickname in nickNames:
+                    if (myList[1] == nickname):
+                        myList.remove(nickname)
+                        message = ""
+                        for i in myList:
+                            message += f"{i} "
+                        print(f"There is a private message to {nickname}")
+                        for WantedClient in client_dick.keys():
+                            print(client_dick[WantedClient])
+                            if client_dick[WantedClient] == nickname:
+                                WantedClient.send(rsa.encrypt(message.encode(),p_key[WantedClient]))
 
-        self.clientSocket.send(rsa.encrypt(self.message.encode(),self.server_key))
-
-        if( self.message[len(self.nickName)+2:] == "EXIT"):
-            self.stop()
-        self.message = ""
-
-    def forPublic(self):
-        self.message += "PUBLIC "
+        except:
+            clients.remove(client)
+            client.close()
+            broadcast(f"{client_dick[client]} has left the chat".encode())
+            break
 
 
-client = Client(serverName, port)
+# basiclly the main function
+def receive():
+    while True:
+
+        # once a client connects get its address and create a socket for them
+        clientSocket, address = serverSocket.accept()
+        print(f"Connected from {address}")
+
+        clientSocket.send(public_key.save_pkcs1("PEM"))
+        p_key[clientSocket] = rsa.PublicKey.load_pkcs1(clientSocket.recv(1024))
+
+        # Sign in or Log in?
+        clientSocket.send(rsa.encrypt("SIGN".encode(),p_key[clientSocket]))
+        first_time = rsa.decrypt(clientSocket.recv(1024),private_key).decode()
+
+        if (first_time == "CREATE ACCOUNT"):
+            # ask them for a username than add them to our lists
+            clientSocket.send(rsa.encrypt("NICK".encode(),p_key[clientSocket]))
+            nickname = rsa.decrypt(clientSocket.recv(1024),private_key).decode()
+            # Used nickname
+            nickname_alredy_used = True
+            if (nickNames.__contains__(nickname)):
+                clientSocket.send(rsa.encrypt("REFUSED".encode(),p_key[clientSocket]))
+                continue
+
+            else:
+                nickname_alredy_used = False
+            nickNames.append(nickname)
+            clients.append(clientSocket)
+            client_dick[clientSocket] = nickname
+
+            # ask them to pick a password
+            clientSocket.send(rsa.encrypt("PASSWORD".encode(),p_key[clientSocket]))
+            password = rsa.decrypt(clientSocket.recv(1024),private_key).decode()
+            passwords[nickname] = password
+
+        else:
+            # ask them their nickname to verify
+            clientSocket.send(rsa.encrypt("NICK".encode(),p_key[clientSocket]))
+            nickname = rsa.decrypt(clientSocket.recv(1024),private_key).decode()
+            if ((nickNames.__contains__(nickname)) and nickname not in banned):
+                # Get password to verify nickname
+                clientSocket.send(rsa.encrypt("PASSWORD".encode(),p_key[clientSocket]))
+                password = rsa.decrypt(clientSocket.recv(1024),private_key).decode()
+                if (passwords[nickname] != password):
+                    clientSocket.send(rsa.encrypt("REFUSED".encode(),p_key[clientSocket]))
+                    clientSocket.close()
+                    continue
+                else:
+                    clients.append(clientSocket)
+            else:
+                clientSocket.send(rsa.encrypt("REFUSED".encode(),p_key[clientSocket]))
+                clientSocket.close()
+                continue
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast("NEW".encode())
+        temp = ""
+        for g in nickNames:
+            temp += f"{g} "
+        broadcast(temp.encode())
+
+        # We will have a new thread for each client we want to handle each connection separately !!!
+        thread = threading.Thread(target=handle, args=(clientSocket,))
+        thread.start()
+
+
+receive()
+serverSocket.close()
+
